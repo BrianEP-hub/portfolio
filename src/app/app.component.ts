f090736f2c37dc835ed00d6962194fa2eaf8e009
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2022-12-09 22:09:12 +0100

    changes to angular.json, readded basehref for deploy script, scroll to top button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'Portfolio';
 
+  windowScrolled: boolean = false;
 
-
-  constructor(){
-
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop > 100
+    ) {
+      this.windowScrolled = true;
+    } else if (
+      (this.windowScrolled && window.pageYOffset) ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop < 10
+    ) {
+      this.windowScrolled = false;
+    }
   }
-  ngOnInit(): void{
 
+  scrollToTop() {
+    (function smoothscroll() {
+      var currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - currentScroll / 8);
+      }
+    })();
   }
+  ngOnInit(): void {}
 }
